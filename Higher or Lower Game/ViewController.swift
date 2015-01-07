12e47b1d569401f1da2b7cd05bdf6c54aeadd705
Marco <Marco@Marcos-iMac.fritz.box>
2015-01-07 20:32:23 +0100

    Anfang

--- Higher or Lower Game/ViewController.swift
+++ Higher or Lower Game/ViewController.swift
@@ -8,18 +8,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController {
+    
+    var number: Int!
+   
+   
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+       
+        
+        number = 30
+        
+        number = Int(arc4random_uniform(100) + 1)
+    
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    
+    @IBAction func guessButton(sender: AnyObject) {
+   
+    let zahl = inputTextField.text.toInt()!
+    
+        inputTextField.text = ""
+    
+    if zahl < number {
+    
+        outputLabel.text = "Meine Zahl ist höher als \(zahl)"
     }
+    else if zahl > number {
+        
+       outputLabel.text = "Meine Zahl ist niedriger als \(zahl)"
+        }
+    else {
+        outputLabel.text = "Du hast die Zahl \(zahl) erraten"
+        }
+    
+    
+    }
+
+
+    @IBAction func resetButtonPressed(sender: AnyObject) {
+    
+    number = Int(arc4random_uniform(100) + 1)
+    outputLabel.text = "Dann noch einmal!"
+    inputTextField.text = ""
+    }
+
 
 
 }
+
+
+
+
+
+
+
 
